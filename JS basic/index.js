49990feca606cb26e49130b2650886ba43da6832
KingOm534 <omkarrlande.534@gmail.com>
2023-03-09 20:04:54 +0530

    js-1

--- JS basic/index.js
+++ JS basic/index.js
@@ -1,2 +1,14 @@
  //following line mainly used for printing statements
  console.log('mashaallah!!');
+ 
+ //Object
+ let rectangle={
+    length: 1,
+    breadth: 2,
+//method (Function in a object)   
+    draw:   function() {
+        console.log('draw');
+    }
+ };
+
+ //Factory function
