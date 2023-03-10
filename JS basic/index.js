0ad2cffb128c52a989580170034c35dff49e8fa0
KingOm534 <omkarrlande.534@gmail.com>
2023-03-10 20:32:35 +0530

    js day 1

--- JS basic/index.js
+++ JS basic/index.js
@@ -1,14 +1,50 @@
  //following line mainly used for printing statements
- console.log('mashaallah!!');
+//  console.log('mashaallah!!');
  
- //Object
- let rectangle={
-    length: 1,
-    breadth: 2,
-//method (Function in a object)   
-    draw:   function() {
-        console.log('draw');
-    }
- };
+//  //Object
+//  let rectangle={
+//     length: 1,
+//     breadth: 2,
+// //method (Function in a object)   
+//     draw:   function() {
+//         console.log('draw');
+//     }
+//  }; 
 
- //Factory function
+// //  Factory function
+// function createRectangle() {
+
+//     return rectangle ={
+//         length: 2,
+//         breadth: 4,
+
+//         draw() {
+//             console.log('Creating reactangle');
+//         }
+//     };
+
+// }
+
+// //Factory call
+// let rectObj1= createRectangle();
+
+// //called
+// rectObj1.length;
+// rectObj1.breadth;
+// rectObj1.draw();
+
+// //Constructor Function
+// function Rectangles(){
+//     this.length=2;
+//     this.breadth=3;
+//     this.draw= function(){
+//         console.log('Drawing');
+//     }
+// }
+// let rectObj2 = new Rectangles();
+
+// rectObj2.color ='RED';
+// console.log(rectObj2);
+
+// delete rectObj2.color;
+// console.log(rectObj2);
